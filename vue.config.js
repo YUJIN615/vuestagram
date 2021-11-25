bfd3ba0eb47aa0d9ad1fea7296998a630c0c4273
yujin <rosepark306@gmail.com>
2021-11-25 17:42:23 +0900

    pwa 설치

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   publicPath: '/vuestagram',
+  pwa: {
+    name: '님 앱이름',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    workboxOptions: {
+      exclude: [/\.map$/, /manifest\.json$/, 'index.html']
+    }
+  }
 }
